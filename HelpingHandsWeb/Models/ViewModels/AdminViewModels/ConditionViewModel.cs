b03d285ec28cd222187f754f4c23340c341dbf2b
pxl123 <lilithamaphini@gmail.com>
2023-10-26 11:04:20 +0200

    Update ConditionViewModel.cs

--- HelpingHandsWeb/Models/ViewModels/AdminViewModels/ConditionViewModel.cs
+++ HelpingHandsWeb/Models/ViewModels/AdminViewModels/ConditionViewModel.cs
@@ -1,7 +1,8 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Data;
-using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using HelpingHandsWeb.Models.Users;
 
 namespace HelpingHandsWeb.Models.ViewModels.AdminViewModels
 {
@@ -9,6 +10,17 @@ namespace HelpingHandsWeb.Models.ViewModels.AdminViewModels
     {
 
 
+        [Key]
+        public int ConditionID { get; set; }
+
+        [Required(ErrorMessage = "Condition Name is required.")]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [StringLength(maximumLength: int.MaxValue)]
+        public string Description { get; set; }
+
+        public bool IsDeleted { get; set; }
 
     }
 }
