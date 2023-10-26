704c6ea4f02682d5d466cfc664caf83056b41836
pxl123 <lilithamaphini@gmail.com>
2023-10-26 17:04:08 +0200

    Update OfficeManagerViewModel.cs

--- HelpingHandsWeb/Models/ViewModels/AdminViewModels/OfficeManagerViewModel.cs
+++ HelpingHandsWeb/Models/ViewModels/AdminViewModels/OfficeManagerViewModel.cs
@@ -7,10 +7,36 @@ namespace HelpingHandsWeb.Models.ViewModels.AdminViewModels
 {
     public class OfficeManagerViewModel
     {
+        public int UserID { get; set; }
 
+        [Required(ErrorMessage = "Username is required.")]
+        public string UserName { get; set; }
 
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Invalid Email Address.")]
+        public string Email { get; set; }
 
+        [Required(ErrorMessage = "Password is required.")]
+        public string Password { get; set; }
+
+        [Compare("Password", ErrorMessage = "Password and Confirm Password do not match.")]
+        public string ConfirmPassword { get; set; }
+
+        [Display(Name = "Contact Number")]
+        public string ContactNo { get; set; }
+
+        [Display(Name = "Profile Picture")]
+        public byte[] ProfilePicture { get; set; }
+
+        [Required(ErrorMessage = "Status is required.")]
+        public string Status { get; set; }
+
+        public OfficeManagerViewModel()
+        {
+            Status = "A";
+        }
     }
+
 }
 
 
