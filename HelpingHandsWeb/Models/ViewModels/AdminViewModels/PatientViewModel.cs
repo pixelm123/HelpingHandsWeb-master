3359468d2d1086e5891f1dccf37dfc7d38164c5f
pxl123 <lilithamaphini@gmail.com>
2023-10-27 16:41:19 +0200

    Update PatientViewModel.cs

--- HelpingHandsWeb/Models/ViewModels/AdminViewModels/PatientViewModel.cs
+++ HelpingHandsWeb/Models/ViewModels/AdminViewModels/PatientViewModel.cs
@@ -1,6 +1,10 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.ComponentModel.DataAnnotations;
 using System.Data.SqlClient;
 
 namespace HelpingHandsWeb.Models.ViewModels.AdminViewModels
