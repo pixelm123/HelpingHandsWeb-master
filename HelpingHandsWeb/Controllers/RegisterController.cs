665e445fb1d133ed974c1fd30e9ae29488d76412
pxl123 <lilithamaphini@gmail.com>
2023-10-30 18:03:41 +0200

    Update RegisterController.cs

--- HelpingHandsWeb/Controllers/RegisterController.cs
+++ HelpingHandsWeb/Controllers/RegisterController.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc;
 using Dapper;
 using Microsoft.Extensions.Configuration;
 using HelpingHandsWeb.Models.ViewModels.AdminViewModels;
@@ -11,73 +11,64 @@ using System.Data.SqlClient;
 using System;
 using System.Threading.Tasks;
 
-namespace HelpingHandsWeb.Controllers
+namespace HelpingHandsWeb.Controllers 
 {
-    public class RegisterController : Controller
+  public class RegisterController: Controller 
+  {
+    private readonly IConfiguration _configuration;
+
+    public RegisterController(IConfiguration configuration) 
     {
-       private readonly IConfiguration _configuration;
-
-         public RegisterController(IConfiguration configuration)
-          {
-            _configuration = configuration;
-          }
-
-       private string ConnectionString
-        {
-           get
-           {
-               return _configuration.GetConnectionString("DefaultConnection");
-           }
-        }
-
-         
-        public IActionResult Register()
-        {
-            var model = new RegisterViewModel(); 
-            return View("~/Views/Home/register.cshtml", model);
-        }
-          [HttpPost("Register")]
-    public IActionResult Register(RegisterViewModel model)
-    {
-        if (ModelState.IsValid)
-        {
-            using (SqlConnection connection = new SqlConnection(ConnectionString))
-            {
-                connection.Open();
-
-               
-                connection.Execute(
-                    "InsertPatient",
-                    new
-                    {
-                        UserName = model.UserName,
-                        Password = model.Password,
-                        Email = model.Email,
-                        ContactNo = model.ContactNo,
-                        UserType = "P", 
-                        Status = "A",
-                        ProfilePicture = (object)DBNull.Value,
-                        FirstName = model.FirstName,
-                        Surname = model.Surname,
-                        Gender = model.Gender,
-                        DateOfBirth = model.DateOfBirth,
-                        EmergencyPerson = model.EmergencyPerson,
-                        EmergencyContactNo = model.EmergencyContactNo
-                    },
-                    commandType: CommandType.StoredProcedure
-                );
-
-                
-            }
-
-            return RedirectToAction("Login", "Home");
-        }
-
-        
-        return View("~/Views/Register.cshtml", model);
+      _configuration = configuration;
     }
 
-
-
+    private string ConnectionString 
+    {
+      get 
+      {
+        return _configuration.GetConnectionString("DefaultConnection");
+      }
     }
+
+    public IActionResult Register() 
+    {
+      var model = new RegisterViewModel();
+      return View("~/Views/Home/register.cshtml", model);
+    } 
+    
+    [HttpPost("Register")]
+    public IActionResult Register(RegisterViewModel model) 
+    {
+      if (ModelState.IsValid) 
+      {
+        using(SqlConnection connection = new SqlConnection(ConnectionString)) 
+        {
+          connection.Open();
+
+          connection.Execute("InsertPatient", new {
+            UserName = model.UserName,
+            Password = model.Password,
+            Email = model.Email,
+            ContactNo = model.ContactNo,
+            UserType = "P",
+            Status = "A",
+            ProfilePicture = (object) DBNull.Value,
+            FirstName = model.FirstName,
+            Surname = model.Surname,
+            Gender = model.Gender,
+            DateOfBirth = model.DateOfBirth,
+            EmergencyPerson = model.EmergencyPerson,
+            EmergencyContactNo = model.EmergencyContactNo
+          },
+          commandType: CommandType.StoredProcedure);
+
+        }
+
+        return RedirectToAction("Login", "Home");
+      }
+
+      return View("~/Views/Register.cshtml", model);
+    }
+
+  }
 }
